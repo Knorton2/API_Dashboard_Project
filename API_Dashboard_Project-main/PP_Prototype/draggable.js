827e6952377f23681e40df5a87df2bfa61dc9f62
Knorton2 <60739954+Knorton2@users.noreply.github.com>
2020-12-07 02:57:08 +0000

    Draggable framework change

---
+++ API_Dashboard_Project-main/PP_Prototype/draggable.js
@@ -0,0 +1,13 @@
+// external js: packery.pkgd.js, draggabilly.pkgd.js
+
+var $grid = $('.grid').packery({
+  itemSelector: '.grid-item',
+  columnWidth: 100
+});
+
+// make all grid-items draggable
+$grid.find('.grid-item').each( function( i, gridItem ) {
+  var draggie = new Draggabilly( gridItem );
+  // bind drag events to Packery
+  $grid.packery( 'bindDraggabillyEvents', draggie );
+});
