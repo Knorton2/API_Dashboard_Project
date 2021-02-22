1891e8de8ee8d10a158dd48e8dbdab28ef82d4fc
Knorton2 <60739954+Knorton2@users.noreply.github.com>
2021-02-22 20:38:10 +0000

    Settings & Cookie Start

---
+++ PP_Prototype/js/settings.js
@@ -0,0 +1,7 @@
+function WeatherSettingsOpen() {
+  document.getElementById("WeatherSettings").style.display = "block";
+}
+
+function WeatherSettingsClose() {
+  document.getElementById("WeatherSettings").style.display = "none";
+}
